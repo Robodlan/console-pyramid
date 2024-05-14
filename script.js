0e0636488866287bc9663fdd9cc8c29d613f1de1
Robodlan <reinaldo.robles@icloud.com>
2024-05-14 19:47:30 -0400

    function padRow

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ for (let i = 0; i < count ; i += 1){
   rows.push(character.repeat(count += i))
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 let result = "";
