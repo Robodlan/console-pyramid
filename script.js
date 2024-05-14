7ac106ee8bbd9ad23f046bbad097454d23b24303
Robodlan <reinaldo.robles@icloud.com>
2024-05-13 21:09:44 -0400

    loop

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const character = 'Hello';
+const character = "#";
 const count = 8;
 const rows = [];
+
+for (let i = 0; i < count ; i += 1){
+  rows.push(character.repeat(count += i))
+}
+console.log(character)
