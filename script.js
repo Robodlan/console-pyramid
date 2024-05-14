fb06e5a8671e939604acdd57b9967c15d72a5ccb
Robodlan <reinaldo.robles@icloud.com>
2024-05-14 19:46:09 -0400

    function

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const rows = [];
 for (let i = 0; i < count ; i += 1){
   rows.push(character.repeat(count += i))
 }
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
 console.log(character)
