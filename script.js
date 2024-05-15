57162f7bda076e3e4665f1bd7874b56ef09921aa
Robodlan <reinaldo.robles@icloud.com>
2024-05-15 15:50:17 -0400

    updated

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(rowNumber, rowCount){
+ return character.repeat(rowNumber)
+}
+
 for (let i = 0;i < count ;i += 1) {
   rows.push(character.repeat(i))
 }
